4d6c5cb29203ac5c0ffaacaab1d11e2842f8bd03
cureband <40882049+cureband@users.noreply.github.com>
2018-07-07 18:56:14 +0800

    Update

---
+++ new.py
@@ -0,0 +1,10 @@
+import time
+import os
+
+bpm = 41
+
+if 40 < bpm < 180:
+       os.system(f"./home/pi/Desktop/Scripts/{bpm}.sh")
+       
+else:
+       os.system(f"./home/pi/Desktop/Scripts/{bpm}.sh")
